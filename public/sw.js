d56bd32e6cd38575cf58c54369d4010b6bb76d75
Dennisss <denzeldenisq@gmail.com>
2022-10-06 13:55:34 +0300

    fixes

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,7 @@
-// import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
-// import { clientsClaim } from 'workbox-core'
-
-// self.skipWaiting()
-// clientsClaim()
-// cleanupOutdatedCaches()
-
-// precacheAndRoute(self.__WB_MANIFEST)
-// registerRoute(new NavigationRoute(
-// 	createHandlerBoundToURL('index.html'),
-// 	{ denylist: [/^\/backoffice/] },
-//   ))
-// Establish a cache name
-const cacheName = 'MyFancyCacheName_v1';
-
 self.addEventListener('fetch', (event) => {
   // Check if this is a request for an image
-if (event.request.destination === 'image') {
-	console.log(event.request)
-    event.respondWith(caches.open(cacheName).then((cache) => {
+if (event.request.destination === 'image' || event.request.destination === 'font') {
+    event.respondWith(caches.open('MyFancyCacheName_v1').then((cache) => {
       // Go to the cache first
     return cache.match(event.request.url).then((cachedResponse) => {
         // Return a cached response if we have one
@@ -34,7 +18,28 @@ if (event.request.destination === 'image') {
         });
     });
     }));
-} else {
+}
+if (event.request.url.endsWith('.scss')) {
+    event.respondWith(caches.open('staticPrecache').then((cache) => {
+      // Go to the cache first
+    return cache.match(event.request.url).then((cachedResponse) => {
+        // Return a cached response if we have one
+        if (cachedResponse) {
+        return cachedResponse;
+        }
+        // Otherwise, hit the network
+        return fetch(event.request).then((fetchedResponse) => {
+          // Add the network response to the cache for later visits
+        cache.put(event.request, fetchedResponse.clone());
+
+          // Return the network response
+        return fetchedResponse;
+        });
+    });
+    }));
+}
+
+else {
     return;
 }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -9,68 +9,8 @@ export default defineConfig({
   plugins: [
     react(),
     viteCompression(),
-    VitePWA({
-      strategies: 'injectManifest',
-      registerType: 'autoUpdate',
-      // workbox: {
-      //   globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      // },
-      devOptions: {
-        enabled: true,
-        // type: 'module',
-      },
-      includeManifestIcons: false,
-      injectRegister: 'auto',
-  }),
   ],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 })
-// manifest: {
-//   name: "movieSearchApp", 
-//   short_name: "MoviePwa",
-//   theme_color: "#141414",
-//   background_color: "#141414",
-//   display: "standalone",
-//   orientation: "portrait",
-//   scope: '/',
-//   start_url: "/?source=pwa",
-//   icons: [
-//     {
-//       src: "icons/512.png",
-//       sizes: "512x512",
-//       type: "image/png"
-//     },
-//     {
-//       src: "icons/256.png",
-//       sizes: "256x256",
-//       type: "image/png"
-//     },
-//     {
-//       src: "icons/192.png",
-//       sizes: "192x192",
-//       type: "image/png"
-//     },
-//     {
-//       src: "icons/and-192.png",
-//       sizes: "192x192",
-//       type: "image/png"
-//     },
-//     {
-//       src: "icons/120.png",
-//       sizes: "120x120",
-//       type: "image/png"
-//     },
-//     {
-//       src: "icons/and-96.png",
-//       sizes: "96x96",
-//       type: "image/png"
-//     },
-//     {
-//       src: "icons/72.png",
-//       sizes: "72x72",
-//       type: "image/png"
-//     }
-//   ]
-// },
