1569dc71beee2607efc146c14579e8d03e74706b
Dennisss <denzeldenisq@gmail.com>
2022-10-09 17:45:34 +0300

    fixes

--- src/Entities/film/ui/review/ui/statistic/index.tsx
+++ src/Entities/film/ui/review/ui/statistic/index.tsx
@@ -1,7 +1,6 @@
-import { useGetReviewByIdQuery } from "../../../../../../shared/api/apiConfig"
-import { getPercent } from "../../../../../../shared/lib"
-import { MovieType } from "../../../../../../types/MovieType"
-import { useVotes } from "../../../../lib"
+import { useGetReviewByIdQuery } from "@/shared/api/apiConfig"
+import { getPercent } from "@/shared/lib"
+import { MovieType } from "@/types/MovieType"
 import style from './styles.module.scss'
 export const StatisticItem = ({total, id}: {total: number, id: string | undefined}) => {
 
@@ -10,23 +9,23 @@ export const StatisticItem = ({total, id}: {total: number, id: string | undefine
 	const badReview = data?.docs?.filter((item:MovieType) =>  item.type === 'Негативный').length
 	const neutralReview = data?.docs?.filter((item:MovieType) =>  item.type === 'Нейтральный').length
 	const items = [
-		{styleReview: 'votes__allNumbers', value: total},
-		{styleReview: 'votes__likedNumber', value: goodReview},
-		{styleReview: 'votes__dislidedNumber', value: badReview},
-		{styleReview: 'votes__neutralNumber', value: neutralReview},
+		{styleReview: 'votes__allNumbers', value: total, subText: 'Всего'},
+		{styleReview: 'votes__likedNumber', value: goodReview, subText: 'Положительные'},
+		{styleReview: 'votes__dislidedNumber', value: badReview, subText: 'Отрицательные'},
+		{styleReview: 'votes__neutralNumber', value: neutralReview, subText: 'Нейтральные'},
 	]
 	// useVotes(data, total)
 	return (
 		<div className={style.statistics}>
 			<div className={style.stickyBlock}>
-				{items.map(({styleReview, value}) => {
+				{items.map(({styleReview, value,subText}) => {
 				return (
 					<div key={styleReview} className={style.review__statistic}>
 						<div className={style.votes}>
 							<div className={style[styleReview]}>{value}</div>
 							<div className={style.votes__precent}>{getPercent({total,value})}</div>
 						</div>
-						<p className={style.undertext}>Всего</p>
+						<p className={style.undertext}>{subText}</p>
 					</div>
 				)
 			})}
