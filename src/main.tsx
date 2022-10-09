917ccd3e1dc456b85647254d2ea198031c53397f
Dennisss <denzeldenisq@gmail.com>
2022-10-09 17:47:04 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'
+import './index.scss'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
    <App />
